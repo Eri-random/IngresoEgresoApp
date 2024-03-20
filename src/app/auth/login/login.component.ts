55b356dcd3082eaee1bbaf5efc1ebc1896dc2fcf
Erika <brenherrera@alumno.unlam.edu.ar>
2024-03-20 19:26:03 -0300

    agrego sweet alert para manejo de errores

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -26,13 +27,26 @@ export class LoginComponent implements OnInit{
   loginUsuario(){
 
     if(this.loginForm.invalid) return;
+
+    Swal.fire({
+      title: "Espere por favor!",
+      didOpen: () => {
+        Swal.showLoading();
+      }
+    });
     
     const {email,password} = this.loginForm.value;
     this.authService.loginUsuario(email,password)
       .then(credenciales =>{
+        Swal.close();
         this.router.navigate(['/']);
       }).catch(err =>{
-        console.error(err);
+        console.log(err);
+          Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: err.message,
+        });
       })
   }
 
