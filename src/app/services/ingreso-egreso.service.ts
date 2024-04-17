2875796b8814a632fd06c37d164e76c6c2a543cc
Erika <brenherrera@alumno.unlam.edu.ar>
2024-04-17 11:50:52 -0300

    arreglo con data ingreso-egreso

--- src/app/services/ingreso-egreso.service.ts
+++ src/app/services/ingreso-egreso.service.ts
@@ -2,7 +2,7 @@ import { Injectable, inject } from '@angular/core';
 import { collection, doc, getDocs, setDoc } from 'firebase/firestore';
 import { IngresoEgreso } from '../models/ingreso-egreso.model';
 import { AuthService } from './auth.service';
-import { Firestore } from '@angular/fire/firestore';
+import { Firestore, onSnapshot } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
@@ -26,16 +26,12 @@ export class IngresoEgresoService {
 
   async initIngresosEgresosListener(uid:string){
     const collectionRef = collection(this.firestore, `${uid}/ingresos-egresos/items`);
-    const querySnapshot = await getDocs(collectionRef);
-    
-    const dataArray: any[] = [];
-    querySnapshot.forEach((doc) => {
-        // Obtener los datos del documento
-        const data = doc.data();
-        dataArray.push(data);
-    });
-
-    console.log(dataArray);
-
+    onSnapshot(collectionRef, (querySnapshot) => {
+      return querySnapshot.docChanges().map(({doc}) => ({
+          uid: doc.id,
+          ...doc.data()
+        })
+      )
+    })
   }
 }
